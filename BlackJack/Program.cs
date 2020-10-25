fba45cd5223fcad79025aafc63fad1734f078e86
LaurieSue <pink.bee.me@outlook.com>
2020-10-24 19:00:59 -0500

    inheritance lesson one and two

--- BlackJack/Program.cs
+++ BlackJack/Program.cs
@@ -10,22 +10,26 @@ namespace BlackJack
     {
         static void Main(string[] args)
         {
-                                    
+               Game game = new Game();
+               game.Dealer = "Jesse";
+               game.Name = "Twenty-One";
              Deck deck = new Deck();  //create deck of cards / instanciate a  new deck object
+             deck.Shuffle(3); // shuffle that deck 3 times
+           
 
-            deck.Shuffle(3); //Shuffle method called , with out param used to keep track of how many times shuffled,  and 3 for  three times to shuffle deck
+           // deck.Shuffle(3); //Shuffle method called , with out param used to keep track of how many times shuffled,  and 3 for  three times to shuffle deck
 
             foreach (Card card in deck.Cards) //foreach loop used to display the deck of cards on the console
             {
                 Console.WriteLine(card.Face + " of " + card.Suit); //format the way to display the cards
             }
-            Console.WriteLine(deck.Cards.Count); //actually write to console the count of cards in the deck ( should be 52 /deck)
+            Console.WriteLine(deck.Cards.Count); //actually write to console the count of cards in the deck ( should be 52 /deck;
             Console.ReadLine();
         }
 
+
+
         
-      
-     //   public static Deck Shuffle(Deck deck, int times)
        // {
            // for (int i = 0; i< times; i++) 
            // {
